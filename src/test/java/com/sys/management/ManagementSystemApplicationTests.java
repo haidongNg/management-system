3be4ba136583535c3e721be37c5eefbe07eae50e
haidongNg <nguyenhaidong1412@gmail.com>
2025-06-15 00:28:08 +0700

    first commit

---
+++ src/test/java/com/sys/management/ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sys.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
